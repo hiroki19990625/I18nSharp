6390be67d7e9155f74e7bdec4f228640b42d809e
hiroki19990625 <hiroki19990625@gmail.com>
2019-10-13 21:22:06 +0900

    Unityサポートの修正

--- I18nSharp.UnityEditor/LanguageFileEditorWindow.cs
+++ I18nSharp.UnityEditor/LanguageFileEditorWindow.cs
@@ -14,9 +14,12 @@ namespace I18nSharp.UnityEditor
     {
         private LanguageFileEditorWindowData _data;
 
-        private string _newLanguageCultureField;
+        private string _newLanguageCultureField = "";
         private string _newLanguageCreatePathField = "Assets/Plugins/I18nSharp/Resources/Languages";
 
+        private bool _renameMode;
+        private string _renameItemKey;
+        private string _renameKey;
         private string _createKey = "New Key";
         private string _createValue = "Text";
 
@@ -50,7 +53,8 @@ namespace I18nSharp.UnityEditor
             _data.selectedToolbar = GUILayout.Toolbar(_data.selectedToolbar, new[]
             {
                 "Editor",
-                "New Language File"
+                "New Language File",
+                "Code Generator(TODO)"
             });
 
             if (_data.selectedToolbar == 0)
@@ -58,7 +62,6 @@ namespace I18nSharp.UnityEditor
                 _data.selectedLanguage =
                     EditorGUILayout.ObjectField("LanguageFile", _data.selectedLanguage, typeof(TextAsset), false) as
                         TextAsset;
-
 
                 if (_data.selectedLanguage != null)
                 {
@@ -70,6 +73,7 @@ namespace I18nSharp.UnityEditor
                     _data.languageFileEditorListViewPosition =
                         EditorGUILayout.BeginScrollView(_data.languageFileEditorListViewPosition, GUI.skin.box);
                     {
+                        string removeKey = null;
                         foreach (KeyValuePair<string, LanguageFileContent> valuePair in _languageFile
                             .LanguageFileDictionary
                             .LanguageFileContents)
@@ -79,30 +83,104 @@ namespace I18nSharp.UnityEditor
                             {
                                 EditorGUILayout.LabelField(valuePair.Key);
                                 text.Content = EditorGUILayout.TextArea(text.Content, EditorStyles.textArea);
+
+                                EditorGUILayout.Space();
+
+                                if (!(_renameMode && _renameItemKey == valuePair.Key) &&
+                                    GUILayout.Button("Rename", GUILayout.Width(100)))
+                                {
+                                    _renameMode = true;
+                                    _renameItemKey = valuePair.Key;
+                                    _renameKey = valuePair.Key;
+                                }
+
+                                if (_renameMode && _renameItemKey == valuePair.Key)
+                                {
+                                    EditorGUILayout.LabelField("Rename Item...");
+                                }
+
+                                if (GUILayout.Button("x", GUILayout.Width(25)))
+                                {
+                                    removeKey = valuePair.Key;
+
+                                    GUIUtility.keyboardControl = 0;
+                                }
                             }
 
                             EditorGUILayout.EndHorizontal();
                         }
+
+                        if (removeKey != null)
+                            _languageFile.LanguageFileDictionary.LanguageFileContents.Remove(removeKey);
                     }
                     EditorGUILayout.EndScrollView();
 
-                    EditorGUILayout.BeginVertical(GUI.skin.box);
+                    if (!_renameMode)
                     {
-                        _createKey = EditorGUILayout.TextField("Key", _createKey);
-                        EditorGUILayout.LabelField("Value");
-                        _createValue = EditorGUILayout.TextArea(_createValue);
-                    }
-                    EditorGUILayout.EndVertical();
-
-                    if (GUILayout.Button("Add"))
-                    {
-                        if (_languageFile.LanguageFileDictionary.LanguageFileContents.ContainsKey(_createKey))
+                        EditorGUILayout.BeginVertical(GUI.skin.box);
                         {
-                            EditorUtility.DisplayDialog("Error", "Created Key", "OK");
-                            return;
+                            EditorGUILayout.LabelField("Add Content");
+                            _createKey = EditorGUILayout.TextField("Key", _createKey);
+                            EditorGUILayout.LabelField("Value");
+                            _createValue = EditorGUILayout.TextArea(_createValue);
+                        }
+                        EditorGUILayout.EndVertical();
+
+                        if (GUILayout.Button("Add"))
+                        {
+                            if (_languageFile.LanguageFileDictionary.LanguageFileContents.ContainsKey(_createKey))
+                            {
+                                EditorUtility.DisplayDialog("Error", "Created Key", "OK");
+                                return;
+                            }
+
+                            if (string.IsNullOrWhiteSpace(_createKey))
+                            {
+                                EditorUtility.DisplayDialog("Error", "Invalid Key", "OK");
+                                return;
+                            }
+
+                            GUIUtility.keyboardControl = 0;
+
+                            _languageFile[_createKey] = new LanguageFileText(_createValue);
+                        }
+                    }
+                    else
+                    {
+                        EditorGUILayout.LabelField("Rename Mode");
+
+                        _renameKey = EditorGUILayout.TextField("Rename Key", _renameKey);
+                        if (GUILayout.Button("Rename"))
+                        {
+                            if (_languageFile.LanguageFileDictionary.LanguageFileContents.ContainsKey(_renameKey))
+                            {
+                                EditorUtility.DisplayDialog("Error", "Created Key", "OK");
+                                return;
+                            }
+
+                            if (string.IsNullOrWhiteSpace(_renameKey))
+                            {
+                                EditorUtility.DisplayDialog("Error", "Invalid Key", "OK");
+                                return;
+                            }
+
+                            LanguageFileContent content = _languageFile[_renameItemKey];
+                            _languageFile.LanguageFileDictionary.LanguageFileContents.Remove(_renameItemKey);
+                            _languageFile[_renameKey] = content;
+
+                            GUIUtility.keyboardControl = 0;
+
+                            _renameMode = false;
+                            _renameItemKey = null;
                         }
 
-                        _languageFile[_createKey] = new LanguageFileText(_createValue);
+                        if (GUILayout.Button("Cancel"))
+                        {
+                            GUIUtility.keyboardControl = 0;
+
+                            _renameMode = false;
+                            _renameItemKey = null;
+                        }
                     }
 
                     if (GUILayout.Button("Save"))
@@ -122,6 +200,8 @@ namespace I18nSharp.UnityEditor
                 {
                     string[] cultures = CultureInfo.GetCultures(CultureTypes.AllCultures)
                         .Select(culture => culture.Name)
+                        .Where(cName =>
+                            cName.IndexOf(_newLanguageCultureField, StringComparison.CurrentCultureIgnoreCase) != -1)
                         .ToArray();
 
                     GenericMenu menu = new GenericMenu();
